c635f36fe0e03bed83d8e8bf90d7b29d19ccc0c9
Aditya Gupta <adityasgupta0412@gmail.com>
2024-05-18 19:30:53 +0530

    Many Features

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function GameButton({handlePlay}){
     );
 }
 
-function Footer({handleHowToPlay}) {
+function Footer({handleHowToPlay, handleLeaderBoard}) {
     return (
         <div className={"options"}>
 
@@ -42,7 +42,7 @@ function Footer({handleHowToPlay}) {
 
             <div className={"gameplayOptions"}>
                 <button>
-                    <img src="/src/assets/5898866_competition_leaderboard_rank_ranking_icon.png"/>
+                    <img src="/src/assets/5898866_competition_leaderboard_rank_ranking_icon.png" onClick={handleLeaderBoard}/>
                 </button>
 
                 <button>
@@ -53,23 +53,78 @@ function Footer({handleHowToPlay}) {
     );
 }
 
-function HowToPlay({handleHowToPlay}){
-    return(
-        <div className={"howtoplay"}>
-            <div className={"closeButton"}>
-                <img src="/src/assets/9110796_x_icon.png" onClick={handleHowToPlay}/>
-            </div>
+function HowToPlay({handleHowToPlay}) {
+    return (
+        <div className={"background"}>
+            <div className={"howtoplay"}>
+                <div className={"closeButton"}>
+                    <img src="/src/assets/9110796_x_icon.png" onClick={handleHowToPlay}/>
+                </div>
 
-            <div className={"titleMark"}>
-                <img src="/src/assets/question-sign.png"/>
-            </div>
+                <div className={"titleMark"}>
+                    {/*<img src="/src/assets/question-sign.png"/>*/}
+                    <h1>How To Play</h1>
+                </div>
 
-            <div className={"situationImage"}>
-                <img src="/src/assets/Screen_Shot_2018-10-25_at_11.02.15_AM.jpg"/>
-            </div>
+                <div className={"situationImage"}>
+                    <img src="/src/assets/pikachu-thinking.png"/>
+                </div>
 
-            <div className={"howtoplayDescription"}>
-                <p><strong>Start by selecting a difficulty level. Choose Pokémon cards from the grid, remembering your picks. Avoid duplicates to conquer your memory quest!</strong></p>
+                <div className={"howtoplayDescription"}>
+                    <h1>Don&apos;t Click On the Same Card Twice</h1>
+                    <h1>Keep On going To Score Higher Points</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function LeaderBoards({handleLeaderBoard}) {
+    const [leaderboard, setLeaderboard] = useState([["Red", 10], ["Ash", 8], ["Misty", 6], ["Brock", 9]]);
+
+    return (
+        <div className={"background"}>
+            <div className={"leaderboards"}>
+                <div className={"closeButton"}>
+                    <img src="/src/assets/9110796_x_icon.png" onClick={handleLeaderBoard}/>
+                </div>
+                <div className={"titleMark leaderboards-title"}>
+                    <h1>LEADERBOARDS</h1>
+                </div>
+
+                <div className={"situationImage leaderboards-situation"}>
+                    <img src="/src/assets/Angry-Pikachu-PNG-Image.png"/>
+                </div>
+
+                <div className="leaderboard-table">
+                    <table>
+                        <tr>
+                            <td>
+                                <h2>Name</h2>
+                            </td>
+                            <td>
+                                <h2>Score</h2>
+                            </td>
+                        </tr>
+
+                        {leaderboard.sort((a, b) => {
+                            return b[1] - a[1]
+                        }).slice(0, 3).map((player) => {
+                            return (
+                                <tr>
+                                    <td>
+                                        <h3>{player[0]}</h3>
+                                    </td>
+                                    <td>
+                                        <h3>{player[1]}</h3>
+                                    </td>
+                                </tr>
+                            );
+                        })
+                        }
+
+                    </table>
+                </div>
             </div>
         </div>
     );
@@ -81,8 +136,14 @@ export default function Game() {
 
     const [howToPlayState, setHowToPlayState] = useState(false);
 
-    const handleHowToPlay = () =>{
+    const [leaderBoardState, setLeaderBoardState] = useState(false);
+
+    const handleHowToPlay = () => {
         setHowToPlayState(!howToPlayState);
+    }
+
+    const handleLeaderBoard = () => {
+        setLeaderBoardState(!leaderBoardState);
     }
     const handleGameState = () => {
         if (gameState === 0) {
@@ -108,10 +169,11 @@ export default function Game() {
 
             { howToPlayState && <HowToPlay handleHowToPlay={handleHowToPlay}/>}
 
+            { leaderBoardState && <LeaderBoards handleLeaderBoard={handleLeaderBoard}/>}
+
             {handleGameState()}
 
-            <Footer handleHowToPlay={handleHowToPlay}/>
-
+            <Footer handleHowToPlay={handleHowToPlay} handleLeaderBoard={handleLeaderBoard}/>
 
         </>
     )
